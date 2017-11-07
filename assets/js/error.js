146f8f157ae25d4eebdbd35814cc2573904e52c8
leet.don <brandon@leetdigital.com>
2017-11-07 09:47:50 +0800

    preparing template frontend for rch

---
+++ assets/js/error.js
@@ -0,0 +1,26 @@
+// BASE FILE IMPORT
+// import the "_base.js" file here:
+// @codekit-prepend '_base.js'
+
+
+// PAGE FRAMEWORK COMPONENT IMPORT
+// import any framework components that are used in this page only:
+// @codekit-prepend '../bower_components/uikit/js/components/slideshow.js'
+
+
+// PAGE FUNCTIONS
+// define needed page functions here, when document.readyState === "complete":
+
+/* if jQuery IS included in the project, use this: */
+$(function() {
+    
+    /* page functions go here */
+    
+});
+
+/* if jQuery IS NOT included in the project, use this instead: */
+document.addEventListener('DOMContentLoaded', function() {
+   
+   /* page functions go here */
+   
+});
