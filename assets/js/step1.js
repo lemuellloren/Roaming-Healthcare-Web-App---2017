2b16a319a92f8351a3f36acf76397d5550f7a79d
lemuel <lemuel@leetdigital.com>
2017-11-20 17:33:52 +0800

    Step 2 Tab Desktop fixed

--- assets/js/step1.js
+++ assets/js/step1.js
@@ -38,7 +38,27 @@ $(document).ready(function() {
         e.preventDefault(); $(this).parent('div').remove(); x--;
     })
 });
+
+//wrap 2
+
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input-fields-wrap2"); //Fields wrapper
+    var add_button      = $(".add-field-button2"); //Add button ID
     
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field2">X Remove</a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field2", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
  
 })(jQuery);
 
