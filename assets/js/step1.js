ace8fd612b22ce014935a78089cbbc28faa521b0
lemuel <lemuel@leetdigital.com>
2017-11-20 16:27:31 +0800

    Step 2 Tab set up

--- assets/js/step1.js
+++ assets/js/step1.js
@@ -20,7 +20,24 @@ $("#upload-icon").click(function () {
     $("#upload-file").trigger('click');
 });
 
-
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $(".input-fields-wrap"); //Fields wrapper
+    var add_button      = $(".add-field-button"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">X Remove</a></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
     
  
 })(jQuery);
